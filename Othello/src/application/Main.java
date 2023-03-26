8338688d84287b50c362dc113332e99c1f67cd2f
M.T <M.T@192.168.10.109>
2023-03-26 19:30:56 +0900

    2回目のコミット
    試しにタイトルだけ変えました

--- Othello/src/application/Main.java
+++ Othello/src/application/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(false);
-			primaryStage.setTitle("オセロでござる");
+			primaryStage.setTitle("オセロ");
 			primaryStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
